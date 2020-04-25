7b15758426da00a5334765e6114bb5ed5d5fd173
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2020-04-25 17:11:47 +0200

    reload button

--- src/features/dashboard/Settings.tsx
+++ src/features/dashboard/Settings.tsx
@@ -6,12 +6,19 @@ import CountryPickList from "./CountryPickList";
 import styles from "./Settings.module.scss";
 import { Toolbar } from "primereact/toolbar";
 import classNames from "classnames";
-import { useDispatch } from "react-redux";
-import { resetCountries } from "./coronaSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { resetCountries, fetchCountryHistoricData } from "./coronaSlice";
+import { RootState } from "../../app/store";
 
 const Settings: React.FC = () => {
   const [visible, setVisible] = useState(false);
   const dispatch = useDispatch();
+  const { selectedCountries } = useSelector((state: RootState) => state.corona);
+  const reloadCountries = () => {
+    selectedCountries.forEach((country) => {
+      dispatch(fetchCountryHistoricData(country.code));
+    });
+  };
 
   return (
     <>
@@ -37,22 +44,32 @@ const Settings: React.FC = () => {
           tooltip="Select Countries"
           tooltipOptions={{
             position: "bottom",
-            showDelay: 300,
-            hideDelay: 300,
           }}
           onClick={() => setVisible(true)}
         />
 
         <Button
-          label="Reset"
+          label="Reload"
           icon="pi pi-replay"
+          className={classNames(
+            styles.button,
+            "p-button-raised p-button-rounded p-button-success"
+          )}
+          tooltip="Reload current data from server"
+          onClick={reloadCountries}
+          tooltipOptions={{
+            position: "bottom",
+          }}
+        />
+
+        <Button
+          label="Reset"
+          icon="pi pi-times"
           className="p-button-raised p-button-rounded p-button-warning"
           tooltip="Reset country list to defaults"
           onClick={() => dispatch(resetCountries())}
           tooltipOptions={{
             position: "bottom",
-            showDelay: 300,
-            hideDelay: 300,
           }}
         />
       </Toolbar>
